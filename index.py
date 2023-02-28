ebf2a0476a381fda9916133e5007ea047b6438f4
J-Massoda <emilienmassoda@gmail.com>
2023-02-28 05:32:45 +0100

    edit

---
+++ index.py
@@ -0,0 +1,31 @@
+# print('Welcome to python 101 on scrimba')
+
+# Using variables
+
+# failed_subjects = "1"
+
+# name = 'Hohn'
+
+# print('Dear Mrs Badger')
+# print('Your son ' + name + ' Eric is failing ' + failed_subjects + ' subjects.')
+# print(name + ' will need to redo ' + failed_subjects + ' courses.')
+# name = 'Eric'
+# print(name + ' is doing well in geography.')
+
+
+# print('Variables & Datatypes - Exercise')
+# Create appropriate Variables for Item name, the price
+# and how many you have in stock
+
+# name = 'john'
+# age = 20
+# height = '6.5'
+a = 10
+b = 3
+print('Addition : ', a + b)
+print('Subtraction : ', a - b)
+print('Multiplication : ', a * b)
+print('Division (float) : ', a / b)
+print('Division (floor) : ', a // b)
+print('Modulus : ', a % b)
+print('Exponent : ', a ** b)
